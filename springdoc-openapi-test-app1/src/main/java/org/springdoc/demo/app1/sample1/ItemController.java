f284ace18a3ebd2a6ef0469a64374110714b58b7
springdoc <springdoc99@gmail.com>
2019-08-07 04:23:28 +0200

    fixes #14

--- springdoc-openapi-test-app1/src/main/java/org/springdoc/demo/app1/sample1/ItemController.java
+++ springdoc-openapi-test-app1/src/main/java/org/springdoc/demo/app1/sample1/ItemController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.UUID;
 
 import javax.validation.Valid;
+import javax.validation.constraints.Size;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,14 +16,20 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @Tag(name = "items")
 public class ItemController {
 
+
 	@GetMapping("/items")
-	public List<ItemDTO> showItems(@RequestParam("cusID") final String customerID) {
+	public List<ItemDTO> showItems(@RequestParam("cusID") @Size(min = 4, max = 6) final String customerID,
+			@Size(min = 4, max = 6) int toto) {
 		return new ArrayList<ItemDTO>();
 	}
    
@@ -36,5 +43,12 @@ public class ItemController {
 	@GetMapping("/items/demo")
 	public List<ItemDTO> showItemsForObject(final ItemLightDTO itemDTO) {
 		return new ArrayList<ItemDTO>();
+	}
+
+	@GetMapping(value = "/items/empty-params")
+	@Operation(parameters = {
+			@Parameter(name = "name", in = ParameterIn.QUERY, schema = @Schema(implementation = String.class)) })
+	public void get() {
+
 	}
 }
