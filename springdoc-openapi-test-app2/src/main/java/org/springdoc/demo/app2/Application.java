2e30fe597809e02d23ca3096bb66f181a8617653
springdoc <springdoc99@gmail.com>
2019-08-09 22:56:00 +0200

    project review

--- springdoc-openapi-test-app2/src/main/java/org/springdoc/demo/app2/Application.java
+++ springdoc-openapi-test-app2/src/main/java/org/springdoc/demo/app2/Application.java
@@ -1,8 +1,16 @@
 package org.springdoc.demo.app2;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @SpringBootApplication
 @ComponentScan(basePackages = { "org.springdoc.demo.app2" })
@@ -12,4 +20,14 @@ public class Application {
 		SpringApplication.run(Application.class);
 	}
 
+	@Bean
+	public OpenAPI customOpenAPI(@Value("${springdoc.version}") String appVersion) {
+		return new OpenAPI()
+				.components(new Components().addSecuritySchemes("basicScheme",
+						new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("basic")))
+				.info(new Info().title("Petstore API").version(appVersion).description(
+						"This is a sample server Petstore server.  You can find out more about     Swagger at [http://swagger.io](http://swagger.io) or on [irc.freenode.net, #swagger](http://swagger.io/irc/).      For this sample, you can use the api key `special-key` to test the authorization     filters.")
+						.termsOfService("http://swagger.io/terms/")
+						.license(new License().name("Apache 2.0").url("http://springdoc.org")));
+	}
 }
--- springdoc-openapi-test-app3/src/main/java/org/springdoc/demo/app3/WebfluxDemoApplication.java
+++ springdoc-openapi-test-app3/src/main/java/org/springdoc/demo/app3/WebfluxDemoApplication.java
@@ -1,8 +1,16 @@
 package org.springdoc.demo.app3;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @SpringBootApplication
 @EnableReactiveMongoRepositories
@@ -11,4 +19,14 @@ public class WebfluxDemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WebfluxDemoApplication.class, args);
 	}
+
+	@Bean
+	public OpenAPI customOpenAPI(@Value("${springdoc.version}") String appVersion) {
+		return new OpenAPI()
+				.components(new Components().addSecuritySchemes("basicScheme",
+						new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("basic")))
+				.info(new Info().title("Tweet API").version(appVersion)
+						.license(new License().name("Apache 2.0").url("http://springdoc.org")));
+	}
+
 }
