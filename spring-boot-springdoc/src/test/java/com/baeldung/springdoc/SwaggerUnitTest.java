e1364d5991f2647c45cc8489cbb90e1ed23327fd
springdoc <springdoc99@gmail.com>
2019-11-27 20:34:45 +0100

    sample update

--- spring-boot-springdoc/src/test/java/com/baeldung/springdoc/SwaggerUnitTest.java
+++ spring-boot-springdoc/src/test/java/com/baeldung/springdoc/SwaggerUnitTest.java
@@ -10,14 +10,36 @@ import io.restassured.specification.RequestSpecification;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+import static org.junit.Assert.assertTrue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class SwaggerUnitTest extends AbstractRestAssuredSetupClass {
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+
+    @Test
+    public void shouldDisplaySwaggerUiPage() throws Exception {
+        MvcResult mvcResult = mockMvc.perform(get("/swagger-ui/index.html")).andExpect(status().isOk()).andReturn();
+        String contentAsString = mvcResult.getResponse().getContentAsString();
+        assertTrue(contentAsString.contains("Swagger UI"));
+    }
     /**
      * Test successful swagger-UI call
      **/
