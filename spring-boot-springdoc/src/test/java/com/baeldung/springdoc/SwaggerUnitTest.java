1a371b242d23a7335a31fa7c07be76ac0bf3c9c2
springdoc <springdoc99@gmail.com>
2019-11-27 20:44:39 +0100

    sample update

--- spring-boot-springdoc/src/test/java/com/baeldung/springdoc/SwaggerUnitTest.java
+++ spring-boot-springdoc/src/test/java/com/baeldung/springdoc/SwaggerUnitTest.java
@@ -1,20 +1,10 @@
 package com.baeldung.springdoc;
 
-import io.restassured.RestAssured;
-import io.restassured.config.RedirectConfig;
-import io.restassured.config.RestAssuredConfig;
-import io.restassured.filter.log.RequestLoggingFilter;
-import io.restassured.filter.log.ResponseLoggingFilter;
-import io.restassured.http.ContentType;
-import io.restassured.specification.RequestSpecification;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpStatus;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -27,12 +17,11 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 @ActiveProfiles("test")
 @AutoConfigureMockMvc
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class SwaggerUnitTest extends AbstractRestAssuredSetupClass {
+@SpringBootTest
+public class SwaggerUnitTest  {
 
     @Autowired
     protected MockMvc mockMvc;
-
 
     @Test
     public void shouldDisplaySwaggerUiPage() throws Exception {
@@ -40,41 +29,5 @@ public class SwaggerUnitTest extends AbstractRestAssuredSetupClass {
         String contentAsString = mvcResult.getResponse().getContentAsString();
         assertTrue(contentAsString.contains("Swagger UI"));
     }
-    /**
-     * Test successful swagger-UI call
-     **/
-    @Test
-    public void getSwaggerUIPage() {
-        givenAnAdministratorIsAuthenticated()
-                .contentType(ContentType.HTML)
-                .get("/swagger-ui/index.html?url=/v3/api-docs")
-                .then()
-                .statusCode(HttpStatus.OK.value());
-    }
-}
-
-abstract class AbstractRestAssuredSetupClass {
-
-    @Value("${request.logging.enabled:true}")
-    private boolean logRequests;
-    @Value("${response.logging.enabled:true}")
-    private boolean logResponses;
-    @Value("${local.server.port}")
-    private int port;
-
-    @Before
-    public void setUpRestAssured() {
-        RestAssured.baseURI = "http://localhost:" + port;
-        RestAssured.config = RestAssuredConfig.config().redirect(RedirectConfig.redirectConfig().followRedirects(false));
-        if (logRequests) {
-            RestAssured.filters(new RequestLoggingFilter());
-        }
-        if (logResponses) {
-            RestAssured.filters(new ResponseLoggingFilter());
-        }
-    }
-
-    protected RequestSpecification givenAnAdministratorIsAuthenticated() {
-        return RestAssured.given().auth().preemptive().basic("user", "user");
-    }
+    
 }
