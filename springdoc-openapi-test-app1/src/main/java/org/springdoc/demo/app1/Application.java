410fae20fba3029d676cc4d7c0b3a118d20e13ef
springdoc <springdoc99@gmail.com>
2019-12-14 10:50:31 +0100

    project update

--- springdoc-openapi-test-app1/src/main/java/org/springdoc/demo/app1/Application.java
+++ springdoc-openapi-test-app1/src/main/java/org/springdoc/demo/app1/Application.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.tags.Tag;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
