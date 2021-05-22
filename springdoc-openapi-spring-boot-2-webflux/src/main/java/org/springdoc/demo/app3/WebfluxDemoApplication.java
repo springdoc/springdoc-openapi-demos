80f81b3e01a949c2d8aa297b4304773afb544d96
bnasslah <badr.nasslahsen@gmail.com>
2021-05-22 15:45:06 +0200

    code review

--- springdoc-openapi-spring-boot-2-webflux/src/main/java/org/springdoc/demo/app3/WebfluxDemoApplication.java
+++ springdoc-openapi-spring-boot-2-webflux/src/main/java/org/springdoc/demo/app3/WebfluxDemoApplication.java
@@ -18,7 +18,6 @@
 
 package org.springdoc.demo.app3;
 
-import io.r2dbc.spi.ConnectionFactory;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
@@ -30,9 +29,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer;
-import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator;
 
 @SpringBootApplication
 public class WebfluxDemoApplication {
@@ -63,16 +59,6 @@ public class WebfluxDemoApplication {
 		String[] packagedToMatch = { "org.springdoc.demo.app3" };
 		return GroupedOpenApi.builder().group("x-stream").pathsToMatch(paths).packagesToScan(packagedToMatch)
 				.build();
-	}
-
-	@Bean
-	ConnectionFactoryInitializer initializer(ConnectionFactory connectionFactory) {
-
-		ConnectionFactoryInitializer initializer = new ConnectionFactoryInitializer();
-		initializer.setConnectionFactory(connectionFactory);
-		initializer.setDatabasePopulator(new ResourceDatabasePopulator(new ClassPathResource("schema.sql")));
-
-		return initializer;
 	}
 
 }
