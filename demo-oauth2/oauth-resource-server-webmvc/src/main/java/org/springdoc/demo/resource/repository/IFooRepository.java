b168a97240e4a5054ae884c75fd3948879c1fd34
bnasslah <badr.nasslahsen@gmail.com>
2022-04-03 12:46:49 +0200

    Upgrade to spring-boot 3.0.0-M2

--- demo-oauth2/oauth-resource-server-webmvc/src/main/java/org/springdoc/demo/resource/repository/IFooRepository.java
+++ demo-oauth2/oauth-resource-server-webmvc/src/main/java/org/springdoc/demo/resource/repository/IFooRepository.java
@@ -2,7 +2,8 @@ package org.springdoc.demo.resource.repository;
 
 import org.springdoc.demo.resource.model.Foo;
 
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface IFooRepository extends PagingAndSortingRepository<Foo, Long> {
+public interface IFooRepository extends PagingAndSortingRepository<Foo, Long>, CrudRepository<Foo, Long> {
 }
