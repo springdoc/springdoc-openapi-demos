21954d8ff1f6b21789a05456821d313a7aa8bb3e
Badr.NassLahsen <badr.nasslahsen@gmail.com>
2024-04-01 14:13:47 +0200

    Samples update

--- demo-microservices/gateway-service/src/main/java/org/springdoc/demo/services/gateway/GatewayApplication.java
+++ demo-microservices/gateway-service/src/main/java/org/springdoc/demo/services/gateway/GatewayApplication.java
@@ -1,17 +1,23 @@
 package org.springdoc.demo.services.gateway;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springdoc.core.properties.AbstractSwaggerUiConfigProperties.SwaggerUrl;
 import org.springdoc.core.properties.SwaggerUiConfigParameters;
+import org.springdoc.webflux.ui.SwaggerIndexTransformer;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.gateway.route.RouteDefinition;
 import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Lazy;
 
 import static org.springdoc.core.utils.Constants.DEFAULT_API_DOCS_URL;
 
@@ -19,20 +25,24 @@ import static org.springdoc.core.utils.Constants.DEFAULT_API_DOCS_URL;
 @EnableDiscoveryClient
 public class GatewayApplication {
 
-
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
 
-	public GatewayApplication(RouteDefinitionLocator locator, SwaggerUiConfigParameters swaggerUiConfigParameters) {
-		List<RouteDefinition> definitions = locator.getRouteDefinitions().collectList().block();
+	@Bean
+	@Lazy(false)
+	public Set<SwaggerUrl> apis(RouteDefinitionLocator locator, SwaggerUiConfigParameters swaggerUiConfigParameters) {
 		Set<SwaggerUrl> urls = new HashSet<>();
+		List<RouteDefinition> definitions = locator.getRouteDefinitions().collectList().block();
+		for (RouteDefinition definition : definitions) {
+			System.out.println("id: " + definition.getId() + "  " + definition.getUri().toString());
+		}
 		definitions.stream().filter(routeDefinition -> routeDefinition.getId().matches(".*-service")).forEach(routeDefinition -> {
 			String name = routeDefinition.getId().replaceAll("-service", "");
 			SwaggerUrl swaggerUrl = new SwaggerUrl(name, DEFAULT_API_DOCS_URL+"/" + name, null);
 			urls.add(swaggerUrl);
 		});
 		swaggerUiConfigParameters.setUrls(urls);
+		return urls;
 	}
-	
 }
