e56db0e9829e77825416b27326b1f50450e5768c
Badr.NassLahsen <badr.nasslahsen@gmail.com>
2024-04-01 03:16:09 +0200

    samples update

--- springdoc-openapi-microservices/gateway-service/src/main/java/org/springdoc/demo/services/gateway/GatewayApplication.java
+++ springdoc-openapi-microservices/gateway-service/src/main/java/org/springdoc/demo/services/gateway/GatewayApplication.java
@@ -1,5 +1,6 @@
 package org.springdoc.demo.services.gateway;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -7,6 +8,7 @@ import java.util.Set;
 import javax.annotation.PostConstruct;
 
 import org.springdoc.core.AbstractSwaggerUiConfigProperties.SwaggerUrl;
+import org.springdoc.core.GroupedOpenApi;
 import org.springdoc.core.SwaggerUiConfigParameters;
 import org.springdoc.core.SwaggerUiConfigProperties;
 
@@ -16,6 +18,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.gateway.route.RouteDefinition;
 import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Lazy;
 
 import static org.springdoc.core.Constants.DEFAULT_API_DOCS_URL;
 
@@ -27,14 +31,23 @@ public class GatewayApplication {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
 
-	public GatewayApplication(RouteDefinitionLocator locator, SwaggerUiConfigParameters swaggerUiConfigParameters) {
-		List<RouteDefinition> definitions = locator.getRouteDefinitions().collectList().block();
+	@Bean
+	@Lazy(false)
+	public List<GroupedOpenApi> apis(RouteDefinitionLocator locator, SwaggerUiConfigParameters swaggerUiConfigParameters) {
+		List<GroupedOpenApi> groups = new ArrayList<>();
 		Set<SwaggerUrl> urls = new HashSet<>();
+		List<RouteDefinition> definitions = locator.getRouteDefinitions().collectList().block();
+		for (RouteDefinition definition : definitions) {
+			System.out.println("id: " + definition.getId() + "  " + definition.getUri().toString());
+		}
 		definitions.stream().filter(routeDefinition -> routeDefinition.getId().matches(".*-service")).forEach(routeDefinition -> {
 			String name = routeDefinition.getId().replaceAll("-service", "");
+			GroupedOpenApi groupedOpenApi =GroupedOpenApi.builder().pathsToMatch("/" + name + "/**").group(name).build();
+			groups.add(groupedOpenApi);
 			SwaggerUrl swaggerUrl = new SwaggerUrl(name, DEFAULT_API_DOCS_URL+"/" + name, null);
 			urls.add(swaggerUrl);
 		});
 		swaggerUiConfigParameters.setUrls(urls);
+		return groups;
 	}
 }
