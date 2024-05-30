7e877df9cb60b5953f2059c2f3755de74ee44310
Badr.NassLahsen <badr.nasslahsen@gmail.com>
2024-05-30 09:42:59 +0200

    docs update

--- demo-book-service/src/main/java/org/springdoc/demo/services/book/exception/GlobalControllerExceptionHandler.java
+++ demo-book-service/src/main/java/org/springdoc/demo/services/book/exception/GlobalControllerExceptionHandler.java
@@ -18,25 +18,36 @@
 
 package org.springdoc.demo.services.book.exception;
 
+import java.time.Instant;
+
 import org.springframework.core.convert.ConversionFailedException;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
-public class GlobalControllerExceptionHandler {
+public class GlobalControllerExceptionHandler extends ResponseEntityExceptionHandler {
 
 	@ExceptionHandler(ConversionFailedException.class)
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
-	public ResponseEntity<String> handleConnversion(RuntimeException ex) {
-		return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+	public ProblemDetail handleConnversion(RuntimeException e) {
+		ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, e.getMessage());
+		problemDetail.setTitle("Bookmark Not Found");
+		problemDetail.setProperty("errorCategory", "Generic");
+		problemDetail.setProperty("timestamp", Instant.now());
+		return problemDetail;
 	}
 
 	@ExceptionHandler(BookNotFoundException.class)
 	@ResponseStatus(HttpStatus.NOT_FOUND)
-	public ResponseEntity<String> handleBookNotFound(RuntimeException ex) {
-		return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+	public ProblemDetail handleBookNotFound(RuntimeException e) {
+		ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, e.getMessage());
+		problemDetail.setTitle("Book Not Found");
+		problemDetail.setProperty("errorCategory", "Generic");
+		problemDetail.setProperty("timestamp", Instant.now());
+		return problemDetail;
 	}
 }
