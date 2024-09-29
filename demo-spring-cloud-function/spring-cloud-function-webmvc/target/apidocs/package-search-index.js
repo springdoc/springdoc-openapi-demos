3ab6b83260ccb3dba86f68377263299d75cba53c
Badr.NassLahsen <badr.nasslahsen@gmail.com>
2024-09-29 20:59:43 +0200

    demos update

--- demo-spring-cloud-function/spring-cloud-function-webmvc/target/apidocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.springdoc.demo.services.functions"}];updateSearchResults();
--- demo-spring-cloud-function/spring-cloud-function-webmvc/target/apidocs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.springdoc.demo.services.functions","l":"PersonDTO"},{"p":"org.springdoc.demo.services.functions","l":"SampleApplication"}];updateSearchResults();
