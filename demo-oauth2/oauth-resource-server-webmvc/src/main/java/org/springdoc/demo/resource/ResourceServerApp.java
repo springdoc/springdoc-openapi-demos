de8a022fad8451eef89a17bfc2941732946cf7ba
Badr.NassLahsen <badr.nasslahsen@gmail.com>
2024-10-06 14:56:43 +0200

    moving to spring-boot 3.4.0-M3

--- demo-oauth2/oauth-resource-server-webmvc/src/main/java/org/springdoc/demo/resource/ResourceServerApp.java
+++ demo-oauth2/oauth-resource-server-webmvc/src/main/java/org/springdoc/demo/resource/ResourceServerApp.java
@@ -1,14 +1,7 @@
 package org.springdoc.demo.resource;
 
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
-
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ResourceServerApp {
