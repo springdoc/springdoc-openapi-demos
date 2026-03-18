ca957c1424ed9a6c59dc3ed96c65d993c349ccb2
Badr.NassLahsen <badr.nasslahsen@gmail.com>
2026-03-18 03:52:42 +0100

    Added demo-spring-boot-mcp-authorization-server

--- demo-spring-boot-mcp-authorization-server/src/test/java/org/springdoc/demo/mcp/authorizationserver/ApplicationTests.java
+++ demo-spring-boot-mcp-authorization-server/src/test/java/org/springdoc/demo/mcp/authorizationserver/ApplicationTests.java
@@ -1,10 +1,12 @@
 package org.springdoc.demo.mcp.authorizationserver;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIfSystemProperty;
 
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@EnabledIfSystemProperty(named = "mcp.auth.server.test", matches = "true")
 class ApplicationTests {
 
 	@Test
